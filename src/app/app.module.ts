e4f5c021894ea34abd6f47b987fbe3d6522d42ec
Keshav Singh <keshavsingh4522@gmail.com>
2023-11-28 18:35:33 +0530

    OMKR-1 | Defualt page removed.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { DepartmentComponent } from './department/department.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SignupComponent,
+    SigninComponent,
+    EmployeeComponent,
+    DepartmentComponent
   ],
   imports: [
     BrowserModule,
